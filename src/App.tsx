47cc864ee3ae12ea14edea1d4811055b7c6aa1c4
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-08-09 13:36:06 +0100

    learnt about style module and also the configuration with type script

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.css';
 import { Route, Routes } from 'react-router-dom';
 import Page1 from "./component/user/User"
 
+
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <Routes>
         <Route path='/' element={<Page1/>}/>
       </Routes>
--- src/component/user/User.tsx
+++ src/component/user/User.tsx
@@ -1,5 +1,6 @@
-import React from "react"
-import "./User.css"
+import React from "react";
+import styles from "./style.module.css";
+
 
 const User = () => {
     return(
@@ -20,7 +21,7 @@ interface Props {
 const Job: React.FC<Props> = ({company, salary, position}) => {
     return(
         <div>
-            <h1>{company}</h1>
+            <h1 className={styles.company}>{company}</h1>
             <h1>{salary}</h1>
             <h1>{position}</h1>
         </div>
