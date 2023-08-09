46fe6a72ddc2a59c6b9051ff86996acc3fc5372b
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-08-09 14:33:17 +0100

    module3 update

--- src/component/user/User.tsx
+++ src/component/user/User.tsx
@@ -5,7 +5,7 @@ import styles from "./style.module.css";
 const User = () => {
     return(
         <div>
-            <Job company="Amazon" salary={20000} position="Senior DEV"/>
+            <Job company="Amazon" salary={20000} position="Senior DEV" userAge={userAge}/>
         </div>
     )
 }
@@ -14,16 +14,23 @@ const User = () => {
 interface Props {
     company: string,
     salary: number,
-    position: string
+    position: string,
+    userAge: any 
 }
 
+const age: number = 18
+const userAge = prompt("How old are you? ");
+
+
 // creating a component for Job while pass props argument to make the component dynamic.
-const Job: React.FC<Props> = ({company, salary, position}) => {
+const Job: React.FC<Props> = ({company, salary, position, userAge}) => {
+
     return(
         <div>
-            <h1 className={styles.company}>{company}</h1>
-            <h1>{salary}</h1>
-            <h1>{position}</h1>
+            <h1 className={styles.company}>Company: {company}</h1>
+            <h1>Salary: {salary}</h1>
+            <h1>Position: {position}</h1>
+            <h1>Age: {userAge >= 18 ? userAge : `Sorry you are ${userAge} 😞, this page is meant for an Adult.`}</h1>
         </div>
     )
 }
