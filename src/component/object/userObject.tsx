87100a49dfcb72996d9afa528b26c167dab55ae4
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-08-10 12:43:37 +0100

    updated the objects

--- src/component/object/userObject.tsx
+++ src/component/object/userObject.tsx
@@ -8,26 +8,44 @@ const userObject = () => {
         {name: "Oluwapelumi", status: "Senior DEV", salary: 20000000},
         {name: "Covenant", status: "Senior DEV", salary: 20000000},
         {name: "Miracle", status: "Senior DEV", salary: 20000000},
-        {name: "Promise", status: "Senior DEV", salary: 20000000}
+        {name: "Promise", status: "Senior DEV", salary: 20000000},
+        {name: "Victoria", status: "Senior DEV", salary: 20000000},
+        {name: "David", status: "Senior DEV", salary: 20000000},
+        {name: "Adedayo", status: "Senior DEV", salary: 20000000},
     ]
     console.log(users[0]);
 
+
+    // Main web page return.
   return (
     <div>
         {
             // mapping each of the user listed above.
             users.map((user) => {
                 return(
-                    <div>
-                        <h1 style={{color: "blue"}}>Name: {user.name}</h1>
-                        <p>Status: {user.status}</p>
-                        <p>Salary: {user.salary}</p>
-                    </div>
+                    <User name={user.name} status={user.status} salary={user.salary}/>
                 )
             })
         }
     </div>
   )
+}
+
+// creating an interface for the use component argument.
+interface Props {
+    name: string,
+    status: string,
+    salary: number
+}
+
+const User: React.FC<Props> = ({name, status, salary}) => {
+    return(
+        <div>
+            <h1 style={{color: "blue"}}>Name: {name}</h1>
+            <p>Status: {status}</p>
+            <p>Salary: {salary}</p>
+        </div>
+    )
 }
 
 export default userObject
