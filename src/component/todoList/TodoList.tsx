90cd669b9a84d2c34288f5a83b5a88d57b763c13
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-08-14 16:15:35 +0100

    created my todo-list DELETE button

--- src/component/todoList/TodoList.tsx
+++ src/component/todoList/TodoList.tsx
@@ -2,22 +2,41 @@ import style from "./style.module.css";
 import React, { useState } from 'react'
 
 const TodoList = () => {
-    const [input, setInput] = useState("");
-    const [todoList, setTodoList] = useState<string[]>([]);
+    // screating an interface for my task.
+    interface TodoList{
+        id: number,
+        taskName: string
+    }
 
+    const [todoList, setTodoList] = useState<TodoList[]>([]);
+    const [input, setInput] = useState("");
+    
 
     // this function get the value of the input.
     const getInput = (event: any) => {
+        event.preventDefault();
         const newInput = event.target.value;
         setInput(newInput);
     }
 
     // This function Add the new task to out task list.
     const addTaskToList = () =>{
-        const newTask = [...todoList, input];
-        setTodoList(newTask);
-        setInput('');
+        const newTask: TodoList = {
+            id: Date.now(), // Example of generating a unique ID
+            taskName: input,
+        }
+        setTodoList([...todoList, newTask]);
     }
+
+    // This function is use to delete from our todolist.
+    const taskDelete = (taskId: number) => {
+        const newTodoList = todoList.filter((task) => {
+            return task.id !== taskId
+        });
+
+        setTodoList(newTodoList);
+    }
+
 
   return (
     <div className={style.App}>
@@ -26,15 +45,16 @@ const TodoList = () => {
         {/* This is the input field containner */}
         <div className={style.input}>
             <input onChange={getInput} type="text"/>
-            <button onClick={addTaskToList}>Add Task</button>
+            <button type="submit" onClick={addTaskToList}>Add Task</button>
         </div>
 
         <div className={style.list}>
             {
                 todoList?.map((task) => {
                     return(
-                        <div key={task}>
-                            <p>{task}</p>
+                        <div className={style.task} key={task.id}>
+                            <p>{task.taskName}</p>
+                            <button className={style.Delete} onClick={() => taskDelete(task.id)}>Delete</button>
                         </div>
                     )
                 })
